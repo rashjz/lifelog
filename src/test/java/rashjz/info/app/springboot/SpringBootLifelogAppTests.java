6048b5730ddf68e37c592bb259071e839b0867d0
Rashad Javadov <rashjz@users.noreply.github.com>
2017-08-08 12:15:49 +0200

    Update SpringBootLifelogAppTests.java

--- src/test/java/rashjz/info/app/springboot/SpringBootLifelogAppTests.java
+++ src/test/java/rashjz/info/app/springboot/SpringBootLifelogAppTests.java
@@ -1,16 +1,54 @@
 package rashjz.info.app.springboot;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import rashjz.info.app.springboot.controller.ContentRestController;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootLifelogAppTests {
+	final String BASE_URL = "http://localhost:8080/";
 
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ContentRestController controller;
+
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		controller = mock(ContentRestController.class);
+
+		this.mockMvc.perform(get("/api/test")
+				.accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+				.andExpect(status().isOk())
+//				.andExpect(content().mimeType(MediaType.APPLICATION_JSON)
+				;
 	}
 
 }
